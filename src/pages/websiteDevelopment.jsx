d19a5440e900692b7e7a96ed4fd57b14c8a4aef7
Zain Saleem <zain.saleem155@gmail.com>
2026-05-06 18:39:18 +0500

    why choose us added

--- src/pages/websiteDevelopment.jsx
+++ src/pages/websiteDevelopment.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/navbar'
 import Footer from '../components/Footer'
 import DevelopmentCards from '../components/DevelopmentCards';
+import WhyChooseUs from '../components/WhyChooseUs';
 
 function websiteDevelopment() {
   return (
       <div className="website-development">
         <Navbar/>
         <DevelopmentCards/>
+        <WhyChooseUs/>
         <Footer/>
 
       </div>
